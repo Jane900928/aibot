428d9273a6d618567dc43c4a2f330c5dc8775395
Jane900928 <konglin928@gmail.com>
2025-08-12 00:30:15 +0800

    Update Apollo Client to use direct Worker URL instead of rewrite

--- app/apollo-wrapper.tsx
+++ app/apollo-wrapper.tsx
@@ -2,8 +2,13 @@
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
+// 直接使用 Worker 的完整 URL
+// 部署时请替换为你的实际 Worker URL
+const GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 
+  'https://deepseek-ai-worker.konglin928.workers.dev/graphql'
+
 const client = new ApolloClient({
-  uri: '/api/graphql', // This will be proxied to our Cloudflare Worker
+  uri: GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
 })
 
