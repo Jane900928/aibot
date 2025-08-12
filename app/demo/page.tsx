6593bd52612e67f02ea4ce0eb6ac3f825c4e7de6
Jane900928 <konglin928@gmail.com>
2025-08-12 20:18:57 +0800

    Add demo page to showcase Markdown rendering capabilities

---
+++ app/demo/page.tsx
@@ -0,0 +1,11 @@
+import MarkdownDemo from '../components/MarkdownDemo'
+
+export default function DemoPage() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-8">
+      <div className="container mx-auto px-4">
+        <MarkdownDemo />
+      </div>
+    </main>
+  )
+}
